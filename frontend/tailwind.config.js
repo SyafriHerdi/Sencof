6a9706b33a3c7fe1b10d586e03022b9c0683ec38
Jeremyas Cornelis <jeremyas.cornelis@gmail.com>
2022-11-04 20:24:57 +0700

    slicing navbar
    (-) need more fixing in navbar's color and font size

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ['index.html'],
   theme: {
+    colors: {
+      'defaultColor' : '#F9F8F4',
+      'white': '#fff',
+      'darkBrown' : '#A2816E',
+      'navyBlue' : '#263C6B',
+      'lightNavy' : '#385CAA',
+      'lightBrown' : '#B29E7F',
+      'orange' : '#FE6B3D'
+    },
     extend: {},
   },
   plugins: [],
